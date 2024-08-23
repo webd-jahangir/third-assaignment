bd15ad0fc1c3afc1fcaa458e5a52af63d6a919fb
webd.jahangire <webd.jahangir@gmail.comm>
2024-08-23 17:26:10 +0600

    added my update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
-    
+      colors: {
+        customGreen: '#ABEF5F',
+      },
+      fontFamily: {
+        sans: ['Manrope', 'system-ui', 'sans-serif'],
+      },
+      // Custom class utilities
+      extend: {
+        typography: {
+          DEFAULT: {
+            css: {
+              '.manrope': {
+                fontFamily: '"Manrope", sans-serif',
+                fontOpticalSizing: 'auto',
+                fontStyle: 'normal',
+              },
+            },
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
-
