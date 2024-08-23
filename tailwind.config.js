9f1d43bad6ce4a2ffb34f1f6f499a6c1127170a5
webd.jahangire <webd.jahangir@gmail.comm>
2024-08-23 15:23:31 +0600

    added my update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,6 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
-      backgroundImage: {
-        'header-bg': 'linear-gradient(0deg, rgb(255, 255, 255), rgba(255, 255, 255, 0) 100%)',
-      },
       fontFamily: {
         'manrope': ['Manrope', 'sans-serif'],
       },
