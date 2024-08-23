874f3442b0d034bfafda1892f838d105aee21fd7
webd.jahangire <webd.jahangir@gmail.comm>
2024-08-23 16:30:05 +0600

    added my customgreen color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.js"],
+  content: ["./index.html"],
   theme: {
     extend: {
-      fontFamily: {
-        'manrope': ['Manrope', 'sans-serif'],
-      },
-      colors: {
-        customGreen: '#ABEF5F',
-      },
+    
     },
   },
   plugins: [],
